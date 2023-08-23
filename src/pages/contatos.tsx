d8e8408be3db11ff380f10291c1995c35c3c29f6
Tiago Vieira <tiagosvieira10@gmail.com>
2023-08-23 16:15:52 -0300

    contatos - iniciada

--- src/pages/contatos.tsx
+++ src/pages/contatos.tsx
@@ -1,5 +1,46 @@
-export default function Contatos(){ 
+// import { useState } from 'react';
+import { CardContato, Contato, ContatosContainer } from "@/styles/pages/contatos";
+import Link from "next/link"
+
+export default function Contatos() { 
+  // const [copied, setCopied] = useState(false);
+  // const textToCopy = 'tiagosvieira@gmail.com';
+
+  // const copyToClipboard = () => {
+  //   navigator.clipboard.writeText(textToCopy);
+  //   setCopied(true);
+  //   setTimeout(() => setCopied(false), 2000);
+  // };
+  
+  // return(
+  //   <ContatosContainer>
+  //     <p>{textToCopy}</p>
+  //     <button onClick={copyToClipboard}>
+  //       {copied ? 'Email copiado!' : 'Copiar'}
+  //     </button>
+  //   </ContatosContainer>
+  // )
   return(
-    <h1>Contato</h1>
+    <ContatosContainer>
+      <CardContato>
+        <Contato>
+          <p><span>|</span> Email</p>
+          <h1>tiagosvieira10@gmail.com</h1>
+          <button>copiar</button>
+        </Contato>
+
+        <Contato>
+          <p><span>|</span> Telefone</p>
+          <h1>(87) 9 9243-8502</h1>
+          <button>copiar</button>
+        </Contato>
+
+        <Contato>
+          <p><span>|</span> Linkedin</p>
+          <h1>tiagosvieira10@gmail.com</h1>
+          <button><Link href={'https://www.linkedin.com/in/tiago-svieira/'} target="blank">Ir para Linkedin</Link></button>
+        </Contato>
+      </CardContato>
+    </ContatosContainer>
   )
 }
---
+++ src/styles/pages/contatos.ts
@@ -0,0 +1,16 @@
+import { styled } from "..";
+
+export const ContatosContainer = styled('main',{
+  display: 'flex',
+  maxWidth: 1300,
+  margin: '10rem auto',
+  alignItems: 'center',
+  justifyContent: 'center'
+})
+export const CardContato = styled('section',{
+  display: 'block'
+})
+
+export const Contato = styled('section',{
+  
+})
